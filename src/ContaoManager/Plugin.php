11750d1273ccd903c174b30c421acb553556fd65
Kamil Kuzminski <kamil.kuzminski@codefog.pl>
2017-02-21 14:12:47 +0100

    Load the plugin after ContaoCoreBundle

--- src/ContaoManager/Plugin.php
+++ src/ContaoManager/Plugin.php
@@ -2,6 +2,7 @@
 
 namespace Terminal42\CodeLoginBundle\ContaoManager;
 
+use Contao\CoreBundle\ContaoCoreBundle;
 use Contao\ManagerPlugin\Bundle\BundlePluginInterface;
 use Contao\ManagerPlugin\Bundle\Config\BundleConfig;
 use Contao\ManagerPlugin\Bundle\Parser\ParserInterface;
@@ -15,7 +16,7 @@ class Plugin implements BundlePluginInterface
     public function getBundles(ParserInterface $parser)
     {
         return [
-            BundleConfig::create(Terminal42CodeLoginBundle::class),
+            BundleConfig::create(Terminal42CodeLoginBundle::class)->setLoadAfter([ContaoCoreBundle::class]),
         ];
     }
 }
