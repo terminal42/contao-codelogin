1220e8dc0f70ee5b3ecd9535ae183f956c323677
Andreas Schempp <andreas.schempp@terminal42.ch>
2019-02-07 19:59:24 +0100

    Fixed DCA palette in module config

--- src/Resources/contao/dca/tl_module.php
+++ src/Resources/contao/dca/tl_module.php
@@ -3,11 +3,11 @@
 /**
  * Add palettes
  */
-$GLOBALS['TL_DCA']['tl_module']['palettes']['code_login'] = '{title_legend},name,headline,type;{redirect_legend},code_param,jumpTo;{template_legend:hide},customTpl;{protected_legend:hide},protected;{expert_legend:hide},guests,cssID,space';
+$GLOBALS['TL_DCA']['tl_module']['palettes']['code_login'] = '{title_legend},name,headline,type;{redirect_legend},jumpTo,code_param;{template_legend:hide},customTpl;{protected_legend:hide},protected;{expert_legend:hide},guests,cssID,space';
 
 $GLOBALS['TL_DCA']['tl_module']['fields']['code_param'] = [
     'label'             => &$GLOBALS['TL_LANG']['tl_module']['code_param'],
     'inputType'         => 'text',
-    'eval'              => ['maxlength' => 16, 'tl_class' => 'w50'],
+    'eval'              => ['maxlength' => 16, 'tl_class' => 'clr w50'],
     'sql'               => "varchar(16) NOT NULL default ''",
 ];
