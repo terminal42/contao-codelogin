dbde6575aa4b56d1091ff87430b8ed0e08cd8891
Andreas Schempp <andreas.schempp@terminal42.ch>
2019-02-07 20:02:01 +0100

    Fixed frontend module namespace

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -3,4 +3,4 @@
 /**
  * Frontend modules
  */
-$GLOBALS['FE_MOD']['user']['code_login'] = 'Terminal42\CodeLoginBundle\FrontendModule\CodeLoginModule';
+$GLOBALS['FE_MOD']['user']['code_login'] = 'Terminal42\CodeloginBundle\FrontendModule\CodeLoginModule';
